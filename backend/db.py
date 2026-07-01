d3c647268ee8b82af4a08557c1eed956f8c53ac8
Ezagu <agustincalpedm@gmail.com>
2026-07-01 04:15:28 -0300

    añadiendo api a docker compose

--- backend/db.py
+++ backend/db.py
@@ -3,14 +3,21 @@ import psycopg2
 import psycopg2.extras
 from dotenv import load_dotenv
 
-psycopg2.extras.register_uuid()  # le enseña a psycopg2 a adaptar uuid.UUID -> Postgres UUID
+psycopg2.extras.register_uuid()
 
 load_dotenv()
 
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5433")
+DB_NAME = os.getenv("DB_NAME", "postgres")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+
 def get_connection():
     return psycopg2.connect(
-        dbname="sensores_iot", 
-        user="postgres", 
-        password=os.environ["POSTGRES_PASSWORD"], 
-        port=5433
+        dbname=DB_NAME, 
+        user=DB_USER, 
+        password=DB_PASSWORD, 
+        port=DB_PORT,
+        host=DB_HOST
     )
--- db/init.sql
+++ db/init.sql
@@ -66,3 +66,33 @@ SELECT create_hypertable('mediciones', 'time');
 
 CREATE INDEX idx_mediciones_sensor_time
     ON mediciones (sensor_id, time DESC);
+
+-- POBLAR LAS TABLAS
+
+-- 1. Un tipo de sensor
+INSERT INTO tipos_sensor (nombre, unidad, valor_min, valor_max)
+VALUES ('temperatura', '°C', -10, 50);
+
+-- 2. Un usuario dueño
+INSERT INTO usuarios (nombre, email, password)
+VALUES ('Test User', 'test@test.com', 'hasheado_dummy')
+RETURNING id;
+-- copiá el id que te devuelve y pegalo abajo en \gset o a mano
+
+-- 3. Un dispositivo (reemplazá el UUID del usuario)
+INSERT INTO dispositivos (usuario_id, codigo, nombre, ubicacion)
+VALUES ('67cb4055-5ca5-4718-8819-d5be28572fe1', 'ESP32-001', 'Placa Lote Norte', 'Campo A')
+RETURNING id;
+
+-- 4. Un sensor en ese dispositivo (reemplazá el UUID del dispositivo)
+INSERT INTO sensores (dispositivo_id, tipo_sensor_id, nombre)
+VALUES ('9907a9d6-4378-48ac-85ce-99f34a03d106', 1, 'Sensor Temp 1')
+RETURNING id;
+
+-- 5. Mediciones de prueba (reemplazá el UUID del sensor)
+INSERT INTO mediciones (time, sensor_id, value)
+SELECT
+  now() - (i || ' minutes')::interval,
+  '838de415-964b-41be-86bc-ffff51f7f070',
+  20 + 5 * sin(i / 10.0)  -- valores que oscilan, simulando temperatura real
+FROM generate_series(0, 500) AS i;
