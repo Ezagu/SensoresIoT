f44bb655729816d6676c2077068f48194cbddce7
Ezagu <agustincalpedm@gmail.com>
2026-07-02 03:59:34 -0300

    get dispositivo por id

--- backend/db.py
+++ backend/db.py
@@ -2,6 +2,7 @@ import os
 import psycopg2
 import psycopg2.extras
 from dotenv import load_dotenv
+from contextlib import contextmanager
 
 psycopg2.extras.register_uuid()
 
@@ -13,11 +14,16 @@ DB_NAME = os.getenv("DB_NAME", "postgres")
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 
+@contextmanager
 def get_connection():
-    return psycopg2.connect(
+    conn = psycopg2.connect(
         dbname=DB_NAME, 
         user=DB_USER, 
         password=DB_PASSWORD, 
         port=DB_PORT,
         host=DB_HOST
     )
+    try:
+        yield conn
+    finally:
+        conn.close()
--- backend/routers/dispositivos.py
+++ backend/routers/dispositivos.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from uuid import UUID
 from typing import Optional
+from datetime import datetime
 
 from db import get_connection
 
@@ -14,7 +15,18 @@ class DispositivoCreate(BaseModel):
   ubicacion: Optional[str] = None
   descripcion: Optional[str] = None
 
-@router.post("/")
+class DispositivoOut(BaseModel):
+  id: UUID
+  usuario_id: UUID
+  nombre: Optional[str] = None
+  ubicacion: Optional[str] = None
+  descripcion: Optional[str] = None
+  activo: bool
+  last_seen_at: Optional[datetime]
+  created_at: datetime
+
+
+@router.post("/", response_model=DispositivoOut)
 def create_dispositivo(dispositivo: DispositivoCreate):
   with get_connection() as conn:
     with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
@@ -30,3 +42,13 @@ def create_dispositivo(dispositivo: DispositivoCreate):
         return cur.fetchone()
       except psycopg2.errors.ForeignKeyViolation:
         raise HTTPException(404, "usuario_id no existe")
+
+@router.get("/{dispositivo_id}", response_model=DispositivoOut)
+def get_dispositivo_by_id(dispositivo_id: UUID):
+  with get_connection() as conn:
+    with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
+      cur.execute("SELECT * FROM dispositivos WHERE id = %s", (dispositivo_id,))
+      dispositivo = cur.fetchone()
+      if dispositivo is None:
+        raise HTTPException(404, "dispositivo no existe")
+      return dispositivo
